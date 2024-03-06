5f9ab9c8f0038be91adcaec8270832d4c72e9161
Solaerum <jlandis32123@gmail.com>
2024-03-05 22:39:46 -0500

    Added conversion functionality and scaladoc

--- part_2_code/build.sbt
+++ part_2_code/build.sbt
@@ -3,10 +3,14 @@ val scala3Version = "3.4.0"
 lazy val root = project
   .in(file("."))
   .settings(
+    
     name := "Part_2_Code",
     version := "0.1.0-SNAPSHOT",
 
     scalaVersion := scala3Version,
 
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test,
+
+    //Allow using the brics dependancy
+    libraryDependencies += "dk.brics" % "automaton" % "1.12-4"
   )
--- part_2_code/src/main/scala/Main.scala
+++ part_2_code/src/main/scala/Main.scala
@@ -1,18 +1,58 @@
 //Import dk.bric
-import dk.bric._
+/**
+  * dk.brics.automaton library built for java. See the package link for more details https://www.brics.dk/automaton/doc/dk/brics/automaton/package-summary.html
+  */
+import dk.brics.automaton._
 
-
+/**
+  * Contains all of the problems, this may be remade into a containing class in the future
+  */
 object Theory_Part_2 extends App{
-   def Problem_1(regex: String): Int = {
-   val regGuy = new dk.brics.automaton.RegExp("ab(c|d)*")
-   val tryAutomotan = regGuy.toAutomaton()
-   val stringOut = tryAutomotan.getNumberOfStates()
-   return stringOut
-
-}
-
-  println(Problem_1)
-
   
+  //code for function which takes a regex (string) as an input, and outputs the conversion to DFA
+  /**
+    * Solution for problem one, this method translates a regex into an NFA into a DFA
+    * 
+    *
+    * @param regex The regex as a string
+    * 
+    * @return DFA from the input regex that is not in minimal form
+    * Has optional minimal form (remove comments on line DONT FORGET TO FILL THIS IN WITH THE LINE #!)
+    * 
+    */
+  def Problem_1(regexIn: String): Automaton = {
+
+// Instantiate Regex, built in method to translate strings to 
+    val startingExpression = new dk.brics.automaton.RegExp(regexIn)
+
+// Translate Regex to NFA
+    val Nfa = startingExpression.toAutomaton()
+    val determinism_check = Nfa.isDeterministic()
+    
+    var dfa_states = Nfa.getNumberOfStates()
+    
+    println("The new NFA has: " + dfa_states.toString() + " states")
+    println("Is this this NFA already deterministic?" + determinism_check.toString())
+
+// Translate NFA to DFA
+    Nfa.determinize()
+
+    dfa_states = Nfa.getNumberOfStates()
+
+    println("Translating to DFA, we see it now has: " + dfa_states.toString() + " states")
+    
+
+// Optional mimimization
+    Nfa.minimize()
+    //println("The new DFA has: " + dfa_states.toString() + " states")
+
+    return Nfa
+    
+  }
+  
+  println(Problem_1("a|b*"))
+  println(Problem_1("a|b|ab|cb|a*"))
+  println(Problem_1("ab|()|ba"))
+  println(Problem_1("a|b*"))
 
 }
